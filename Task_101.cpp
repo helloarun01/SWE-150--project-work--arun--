bf1dea5be9471a90d2b7b3369f69b143d8a1eb4d
arun <arunroyarun0506@gmail.com>
2024-05-05 17:33:02 +0600

    add window and renderer initializetion

--- Task_101.cpp
+++ Task_101.cpp
@@ -12,3 +12,36 @@ SDL_Window *window=NULL;
 SDL_Renderer *renderer=NULL;
 const int circle_radius=100;
 
+// write function for window and renderer initializetion
+bool initializeWindow(void)
+{
+    if (SDL_Init(SDL_INIT_VIDEO)<0)
+    {
+        printf("error: sdl failed to initialize\nsdl error is: '%s'\n",SDL_GetError());
+        return false;
+    }
+
+    window=SDL_CreateWindow(
+        "Task_101 :project work- 1st Task",
+        SDL_WINDOWPOS_UNDEFINED,
+        SDL_WINDOWPOS_UNDEFINED,
+        SCREEN_WIDTH,
+        SCREEN_HEIGHT,
+        0
+    );
+
+    if(!window)
+    {
+        printf("error failed to open windoe\nsdl error is :'%s'",SDL_GetError());
+        return false;
+    }
+
+    renderer=SDL_CreateRenderer(window,-1,SDL_RENDERER_ACCELERATED);
+    if(!renderer)
+    {
+        printf("error to create renderer\nsdl error is :'%s'\n",SDL_GetError());
+        return false;
+    }
+
+    return true;
+}
