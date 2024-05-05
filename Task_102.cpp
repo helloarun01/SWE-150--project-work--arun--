6eba078b6260a16458a4bd758f22a79685d535b4
arun <arunroyarun0506@gmail.com>
2024-05-05 18:40:05 +0600

    add process function

--- Task_102.cpp
+++ Task_102.cpp
@@ -48,3 +48,21 @@ bool initializeWindow(void)
 }
 
 
+// to process input
+void processInput(void)
+{
+    SDL_Event event;
+    while (SDL_PollEvent(&event))
+    {
+        switch (event.type)
+        {
+        case SDL_QUIT:
+            gameIsRunning = false;
+            break;
+
+        default:
+            break;
+        }
+    }
+}
+
